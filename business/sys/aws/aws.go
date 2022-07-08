f4991dfff41b9526ef56fce9fc24b1ce846befc5
Mahamadou828 <122121Ms192001>
2022-07-08 18:15:05 +0300

    feat(config): adding configuration support

--- business/sys/aws/aws.go
+++ business/sys/aws/aws.go
@@ -1,1 +1,33 @@
 package aws
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+//Client provides an api to interact with all aws services.
+type Client struct {
+	env     string
+	service string
+	SSM     *SSM
+}
+
+func New(sv, env string) (*Client, error) {
+	//Initiate a new aws session
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region:                        aws.String("eu-west-1"),
+			CredentialsChainVerboseErrors: aws.Bool(true),
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error creating session: %v", err)
+	}
+
+	return &Client{
+		env:     env,
+		service: sv,
+		SSM:     NewSSM(sv, env, sess),
+	}, nil
+}
